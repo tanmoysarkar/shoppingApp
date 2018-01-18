0895a9db2688f05c2ccf5d7b296a89dc942f442f
Tanmoy Sarkar <tnmsarkar96@gmail.com>
2018-01-18 16:25:20 +0530

    Add files via upload

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import config from '../../../config/config'
+import config from '../../config/config'
 var api = config.api.root
 
 class profile extends React.Component{
