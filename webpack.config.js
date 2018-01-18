60f365c8cb4795e67ddb109d8c5748bb6fbe5b74
Tanmoy Sarkar <tnmsarkar96@gmail.com>
2018-01-18 17:25:37 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,21 @@ module.exports = {
             presets:['es2015', 'stage-0','react']
           }
        },
-       {     test: /\.css$/,
+       {
+        // Transform our own .css files with PostCSS and CSS-modules
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      }, {
+        // Do not transform vendor's CSS with CSS-modules
+        // The point is that they remain in global scope.
+        // Since we require these CSS files in our JS or CSS files,
+        // they will be a part of our compilation either way.
+        // So, no need for ExtractTextPlugin here.
+        test: /\.css$/,
         include: /node_modules/,
-            loaders: [
-                        
-                    'style-loader', 
-                    'css-loader',        
-                    ]
-
-        },
+        use: ['style-loader', 'css-loader'],
+      },
        {
           test: /\.html$/,
           loader: "file-loader",
